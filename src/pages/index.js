08909adc853990ed64c8876701d90b072b844845
Samuel Gillet <samuel.p.gillet@gmail.com>
2018-11-10 20:02:24 -0700

    create basic gatsby site

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import band_photo from "../images/band_photo.jpg"
+
+export default () => (
+    <div style={{color: 'purple'}}>
+        <Link to="/contact/">Contact</Link>
+        <Header headerText="Hello Gatsby!" />
+        <p>What a world!</p>
+        <img src={band_photo} alt="" />
+    </div>
+
+)
