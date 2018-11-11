4625ea01e628d4bcd18d14e5abd7a58659cd2894
Samuel Gillet <samuel.p.gillet@gmail.com>
2018-11-10 20:17:50 -0700

    add link to about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import band_photo from "../images/band_photo.jpg"
 export default () => (
     <div style={{color: 'purple'}}>
         <Link to="/contact/">Contact</Link>
+        <span style={{'padding-right': 5}} />
+        <Link to="/about/">About</Link>
         <Header headerText="Hello Gatsby!" />
         <p>What a world!</p>
         <img src={band_photo} alt="" />
